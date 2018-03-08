f6515e1fe1620ec30304288b26657100b7102e84
Lucas Eckman <lucas.eckman@gmail.com>
2018-03-08 16:24:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(m, i) {
   var myArray = [];
-  for (c = 0; c > 0; c++) {
-    myArray.push()
+  for (c = 0; c <= m.length; c++) {
+    myArray.push(m.slice(c) + " plays " + i.slice(c))
   }
 }
